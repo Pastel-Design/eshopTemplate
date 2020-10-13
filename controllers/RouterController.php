ece13bbca7b2a423b41022b560803e930f3fc6c8
kukivac <kukivac@gmail.com>
2020-10-13 23:09:25 +0200

    included minify library
    knihovna automaticky zminifikuje css a js soubory patřící k danému pohledu

--- controllers/RouterController.php
+++ controllers/RouterController.php
@@ -1,5 +1,9 @@
 <?php
 
+use MatthiasMullie\Minify;
+
+require("vendor/autoload.php");
+
 class RouterController extends Controller
 {
     /**
@@ -16,30 +20,36 @@ class RouterController extends Controller
     public function process($params)
     {
         //Pro parsování url na parametry zavoláme funkci parseURL() 
-        $CategoriesManager = new CategoriesManager();
         $parsedURL = $this->parseURL($params[0]);
 
         //Pokud v URL nejsou žádné parametry přesměrujeme na domovskou stránku 
         if (empty($parsedURL[0]))
             $this->reroute('home');
         //Jako aktuálně obsluhovaný kontroler se nastaví první parametr z URL v Camel notaci aby seděl název s případným názvem souboru 
-        $controllerClass = $this->dashToCamel(array_shift($parsedURL)) . 'Controller';
+        $controllerName = $this->dashToCamel(array_shift($parsedURL));
+        $controllerClass =  $controllerName . 'Controller';
         //Pokud soubor s názvem kontroleru existuje, nastaví se název třídy kontroleru jako obsluhovaný kontroler do vlastnosti objektu směrovače
         if (file_exists('controllers/' . $controllerClass . '.php'))
             $this->controller = new $controllerClass;
         //v opačném případě přesměrujeme na chybovou stránku s errorem 404
         else
             $this->reroute('error/404');
+
+        //Knihovna Minify zminimalizuje css a js soubor pro daný pohled
+        $minifier = new Minify\CSS;
+        $minifier->add("styles/".$controllerName.".css","styles/style.css");
+        $minifier->minify("styles/minified/".$controllerName.".min.css");
+        $minifier = new Minify\CSS;
+        $minifier->add("scripts/".$controllerName.".js","scripts/script.js");
+        $minifier->minify("scripts/minified/".$controllerName.".min.js");
         //Vybraný kontroler si zpracuje parametry z URL
         $this->controller->process($parsedURL);
         //do hlavičky se nastaví obsah hlavičky tak jak je vytvořil kontroler 
         $this->data['title'] = $this->controller->head['title'];
         $this->data['description'] = $this->controller->head['description'];
         $this->data['keywords'] = $this->controller->head['keywords'];
-        //navíc se do data pole třídy hlavního kontroleru nastaví kategorie, je to případ v tomhle eshopu, základní součástí frameworku to není
-        //je to kvůli tomu že kategorie se vypisují už v základní šabloně layout
-        $this->data['mainCategories'] = $CategoriesManager->getMainCategories();
-        $this->data['subCategories'] = $CategoriesManager->getAllSubcategories();
+        $this->data['css'] = $controllerName . ".min.css";
+        $this->data['js'] = $controllerName . ".min.js";
         //nastavíme základní layout šablonu
         $this->view = 'layout';
     }
