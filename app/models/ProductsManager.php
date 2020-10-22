762ee429a65f159a9522bc0b42a02023a5d514f4
kukivac <50522557+kukivac@users.noreply.github.com>
2020-10-22 12:05:58 +0200

    began working on sign up

--- app/models/ProductsManager.php
+++ app/models/ProductsManager.php
@@ -11,21 +11,10 @@ namespace app\models;
 class ProductsManager
 {
     /**
-     * @var DbManager $dbManager
-     */
-    private  $dbManager;
-
-    public function __construct()
-    {
-        $this->dbManager = new DbManager();
-    }
-
-    /**
      * @return array
      */
     public function selectAllProducts() : array
     {
-        $sql = "Select * from product;";
-        return $this->dbManager->requestMultiple($sql);
+        return DbManager::requestMultiple("SELECT * FROM product;");
     }
 }
